7c17719c77d46a5b5c002588290497be606ce25e
devin <devinelliotgomez@gmail.com>
2023-02-14 15:47:55 -0600

    configurated server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose
+  .connect(
+    process.env.MONGODB_URI || "mongodb://localhost:27017/social-network-api",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
+
+mongoose.set("debug", true);
+
+app.listen(PORT, () => console.log(`Connected to Port ${PORT}`));
