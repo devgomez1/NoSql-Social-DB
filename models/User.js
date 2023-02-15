91af295a82db6da752d71aaf154a5ecf221a353e
devin <devinelliotgomez@gmail.com>
2023-02-14 18:33:44 -0600

    small fix to user model

--- models/User.js
+++ models/User.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
-    match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+    match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/]
   },
   thoughts: [
     {
